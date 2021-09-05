089d57b935d1b67e46ed4480b59e77e98c02972d
lsx0622@126.com <lsx0622@126.com>
2021-09-05 11:09:37 +0000

    update

---
+++ linuxcode/LinuxCsystem/进程/orphan.cpp
@@ -0,0 +1,20 @@
+#include<unistd.h>
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc,char *argv[]){
+    pid_t pid = fork();
+    if(pid == -1){
+        perror("fork error");
+        exit(1);
+    }else if(pid == 0){
+        while(1){
+            sleep(1);
+            printf("i`m child process parent-pid = %d\n",getppid());
+        }
+    }else if(pid > 0){
+            printf("i`m parent process pid = %d\n",pid);
+            sleep(8);
+    }
+    return 0;
+}
