1a00f4947e14a3b19caa8c4886672fb15fe434e3
lsx0622@126.com <lsx0622@126.com>
2021-09-08 09:12:14 +0000

    update

---
+++ linuxcode/LinuxCsystem/进程通信/pipe.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+void sys_err(const char *str){
+    perror(str);
+    exit(1);
+}
+int main(int argc,char *argv[]){
+    int fd[2],i,ret;
+    pid_t pid;
+    char *str = "hello pipe",buf[1024];
+    i = pipe(fd);
+    if(i == -1){
+        sys_err("pipe error");
+    }
+    pid = fork();
+    if(pid == -1){
+        sys_err("fork error");
+    }else if(pid == 0){
+        close(fd[1]);
+        ret = read(fd[0],buf,1024);
+        write(STDOUT_FILENO,buf,ret);
+        close(fd[0]);
+    }else if(pid > 0){
+        close(fd[0]);
+        write(fd[1],str,strlen(str));
+        sleep(1);
+        close(fd[1]);
+    }
+    return 0;
+}
