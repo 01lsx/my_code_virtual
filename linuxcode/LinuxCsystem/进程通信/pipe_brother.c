c4633dd3f321f4fe7fdbee3d932d86b41bf8c136
lsx0622@126.com <lsx0622@126.com>
2021-09-09 07:30:30 +0000

    update

--- linuxcode/LinuxCsystem/进程通信/pipe_brother.c
+++ linuxcode/LinuxCsystem/进程通信/pipe_brother.c
@@ -1,33 +1,44 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<unistd.h>
-#include<string.h>
+#include<sys/wait.h>
 void sys_err(const char *str){
     perror(str);
     exit(1);
 }
 int main(int argc,char *argv[]){
-    int pipefd[2],ret;
+    int pipefd[2];
+    int ret,i;
     pid_t pid;
     ret = pipe(pipefd);
     if(ret == -1){
         sys_err("pipe error");
     }
-    pid = fork();
-    if(pid == -1){
-        sys_err("fork error");
-    }else if(pid == 0){
+    for(i=0;i<2;i++){
+        pid = fork();
+        if(pid == -1){
+            sys_err("fork error");
+        }if(pid == 0){
+            break;
+        }
+    }
+    if(i == 0){ //兄进程
         close(pipefd[0]);
         dup2(pipefd[1],STDOUT_FILENO);
         execlp("ls","ls",NULL);
         sys_err("execlp ls error");
         close(pipefd[1]);
-    }else if(pid > 0){
+    }else if(i == 1){  //弟进程
         close(pipefd[1]);
         dup2(pipefd[0],STDIN_FILENO);
         execlp("wc","wc","-l",NULL);
         sys_err("execlp wc error");
         close(pipefd[0]);
+    }else if(i == 2){
+        close(pipefd[0]);
+        close(pipefd[1]);
+        wait(NULL);
+        wait(NULL);
     }
     return 0;
 }
---
+++ linuxcode/LinuxCsystem/进程通信/pipe_doublew.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<sys/wait.h>
+#include<string.h>
+
+void sys_err(const char *str){
+    perror(str);
+    exit(1);
+}
+
+int main(int argc,char *argv[]){
+    int pipefd[2],ret,i,n;
+    char buf[1024];
+    pid_t pid;
+    ret = pipe(pipefd);
+    if(ret == -1){
+        sys_err("pipe error");
+    }
+    for(i=0;i<2;i++){
+        pid = fork();
+        if(pid == -1){
+            sys_err("fork error");
+        }else if(pid == 0){
+            break;
+        }
+    }
+    if(i == 0){
+        close(pipefd[0]);
+        write(pipefd[1],"1.hello\n",string("1.hello\n"));
+    }else if(i == 1){
+        close(pipefd[0]);
+        write(pipefd[1],"2.world\n",string("2.world\n"));
+    }else{
+        close(pipefd[1]);
+        n = read(pipefd[0],buf,1024);
+        write(STDOUT_FILENO,buf,n);
+        for(i=0;i<2;i++){
+            wait(NULL);
+        }
+    }
+    return 0;
+}
